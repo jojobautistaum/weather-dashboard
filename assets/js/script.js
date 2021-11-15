3b974c8f26ddd32610a5f37c90f4bc803b862738
Jojo Bautista <gjojob@yahoo.com>
2021-11-14 20:08:15 -0600

    added weather icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,18 @@ var searchedCities = [];
 function findCity(city) {
     var locationUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=b7609117c1b58fc397022fe7414e5f44`;
     fetch(locationUrl).then(function(response) {
-        if (response) {
+        if (response.ok) {
             response.json().then(function(data) {
-                checkWeather(data[0].lat,data[0].lon)
+                if (data){
+                    checkWeather(data[0].lat,data[0].lon);
+                }
+                else {
+                    return alert("Can't find the city.");
+                }
             });
         }
         else {
-            return alert("Can't find the city");
+            return alert("Something went wrong");
         }
     });
 }
@@ -20,15 +25,17 @@ function findCity(city) {
 function checkWeather (latitude, longitude) {
     var weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&limit=5&appid=b7609117c1b58fc397022fe7414e5f44`;
     fetch(weatherUrl).then(function(response) {
-        if (response) {
+        if (response.ok) {
             response.json().then(function(data) {
                 weatherToday(data.current.dt, data.current.temp, data.current.wind_speed, data.current.humidity, data.current.uvi, data.current.weather[0].icon);
                 // Loop through 5-day forecast
                 for (var i = 1; i < 6; i++){
-                    weatherForecast(i,data.daily[i].dt, data.daily[i].temp.day, data.daily[i].wind_speed, data.daily[i].humidity);
+                    weatherForecast(i,data.daily[i].dt, data.daily[i].temp.day, data.daily[i].wind_speed, data.daily[i].humidity, data.daily[i].weather[0].icon);
                 }
                 console.log(data);
                 searchedCities.push(city);
+                console.log(searchedCities);
+                
             });
         }
         else {
@@ -41,15 +48,11 @@ function checkWeather (latitude, longitude) {
 // Show weather info today
 function weatherToday(date, temp, wind, humidity, uv, icon){
     var today = new Date(date * 1000).toLocaleString("en-US",{month: "2-digit", day: "2-digit", year: "numeric"});
-    var wicon = "https://openweathermap.org/img/wn/${icon}@2x.png";
-    const markup = `
-    <span>
-        <img src=${wicon}>
-    </span>`;
-
-    $("now-city").innerHTML = markup;
+    var wicon = `https://openweathermap.org/img/w/${icon}.png`;
+    const imgSrc = `<img src=${wicon}>`;
 
     $("#now-city").text(city + " (" + today + ") " );
+    $("#now-city").append(imgSrc);
     $("#now-temp").text("Temp: " + temp + "\xB0F");
     $("#now-wind").text("Wind: " + wind + " MPH");
     $("#now-humid").text("Humidity: " + humidity + " %");
@@ -62,7 +65,7 @@ function weatherForecast(day, date, temp, wind, humidity, icon){
         var wicon = `https://openweathermap.org/img/w/${icon}.png`;
 
         $("#date-" + day).text(forecastDay);
-        $("#icon-" + day).text(wicon);
+        $("#icon-" + day).attr("src",wicon);
         $("#temp-" + day).text("Temp: " + temp + "\xB0F");
         $("#wind-" + day).text("Wind: " + wind + " MPH");
         $("#hum-" + day).text("Humidity: " + humidity + " %");
