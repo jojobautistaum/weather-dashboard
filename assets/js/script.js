e48950d01a920d0ba6490771eb48615a755173a2
Jojo Bautista <gjojob@yahoo.com>
2021-11-18 23:20:53 -0600

    Change all var declaration into let decalaration

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-var city = "";
-var foundCities = [];
-var searchedEl = document.querySelector("#recent-searches");
-var apiKey = ""
+let city = "";
+let foundCities = [];
+let searchedEl = document.querySelector("#recent-searches");
+let apiKey = ""
 
 // Find the city
 function findCity(city, newSearch) {
-    var locationUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
+    let locationUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
     
     fetch(locationUrl).then(function(response) {
         if (response.ok) {
@@ -39,14 +39,14 @@ function findCity(city, newSearch) {
 
 // Pull the weather info
 function checkWeather (latitude, longitude) {
-    var weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&limit=5&appid=${apiKey}`;
+    let weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&limit=5&appid=${apiKey}`;
     
     fetch(weatherUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
                 weatherToday(data.current.dt, data.current.temp, data.current.wind_speed, data.current.humidity, data.current.uvi, data.current.weather[0].icon);
                 // Loop through 5-day forecast
-                for (var i = 1; i < 6; i++){
+                for (let i = 1; i < 6; i++){
                     weatherForecast(i,data.daily[i].dt, data.daily[i].temp.day, data.daily[i].wind_speed, data.daily[i].humidity, data.daily[i].weather[0].icon);
                 }
             });
@@ -62,10 +62,10 @@ function checkWeather (latitude, longitude) {
 
 // Show weather info today
 function weatherToday(date, temp, wind, humidity, uv, icon){
-    var today = new Date(date * 1000).toLocaleString("en-US",{month: "2-digit", day: "2-digit", year: "numeric"});
-    var wicon = `https://openweathermap.org/img/w/${icon}.png`;
+    let today = new Date(date * 1000).toLocaleString("en-US",{month: "2-digit", day: "2-digit", year: "numeric"});
+    let wicon = `https://openweathermap.org/img/w/${icon}.png`;
     const imgSrc = `<img src=${wicon}>`;
-    var uvIndex = document.querySelector("#now-uv");
+    let uvIndex = document.querySelector("#now-uv");
     
     $("#now-city").text(city + " (" + today + ") " );
     $("#now-city").append(imgSrc);
@@ -90,8 +90,8 @@ function weatherToday(date, temp, wind, humidity, uv, icon){
 
 // Show weather day forecast 
 function weatherForecast(day, date, temp, wind, humidity, icon){
-        var forecastDay = new Date(date * 1000).toLocaleString("en-US",{month: "2-digit", day: "2-digit", year: "numeric"}); 
-        var wicon = `https://openweathermap.org/img/w/${icon}.png`;
+        let forecastDay = new Date(date * 1000).toLocaleString("en-US",{month: "2-digit", day: "2-digit", year: "numeric"}); 
+        let wicon = `https://openweathermap.org/img/w/${icon}.png`;
 
         $("#date-" + day).text(forecastDay);
         $("#icon-" + day).attr("src",wicon);
@@ -102,11 +102,11 @@ function weatherForecast(day, date, temp, wind, humidity, icon){
 
 // Create button for recent search
 function addButton(city) {
-    var searchedBtn = document.createElement("button");
+    let searchedBtn = document.createElement("button");
 
     // Make sure that there is no duplicate city
     if (foundCities.length > 0) {
-        for (var i = 0; i < foundCities.length; i++) {
+        for (let i = 0; i < foundCities.length; i++) {
             if (city === foundCities[i]) {
                 return;
             }
@@ -131,9 +131,9 @@ function cityList (appStarting){
         // Update foundCities array from localStorage if not empty
         if (localStorage.getItem("Found Cities") !== null){
             // Use cities as temp array so we can add the buttons as if it's new
-            var cities = localStorage.getItem("Found Cities").split(",");
+            let cities = localStorage.getItem("Found Cities").split(",");
             // Our App just started let's populate the recent searches buttons
-            for (var i = 0; i < cities.length; i++){
+            for (let i = 0; i < cities.length; i++){
                 addButton(cities[i]);
             }
             // After adding the buttons, populate our foundCities array.
@@ -145,9 +145,9 @@ function cityList (appStarting){
 // We will capitalized first letter of each word
 function capsFirstLetter(){
     city = city.toLowerCase();
-    var cityArr = city.split(" ");
+    let cityArr = city.split(" ");
 
-    for (var i =0; i < cityArr.length; i++){
+    for (let i =0; i < cityArr.length; i++){
         cityArr[i] = cityArr[i][0].toUpperCase() + cityArr[i].slice(1);
     }
     city = cityArr.join(" ");
